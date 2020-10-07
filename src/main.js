f75bf0e4aa502e9293889c9178636ce2c9ababf9
Bence Olle <bence.olle.job@gmail.com>
2020-10-07 11:58:34 +0200

    added Nester routes and named routes

--- src/main.js
+++ src/main.js
@@ -5,15 +5,37 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams -> TeamsList
-    { path: '/teams/:teamId', component: TeamMembers, props: true }, // our-domain.com/users -> UsersList
-    { path: '/users', component: UsersList }, // our-domain.com/users -> UsersList,
-    { path: '/:notFound(.*)', redirect: '/teams' }
+    {
+      name: 'teams',
+      path: '/teams',
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter
+      },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter
+      }
+    },
+    { path: '/:notFound(.*)', redirect: '/teams' } // catch undefined paths
   ]
 });
 
